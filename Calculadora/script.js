ddbfe09c43ed3a1dd287e6774c14bf64f786a09c
Luca <lucacallegari71@gmail.com>
2025-08-14 17:06:07 -0300

    style.css adicionado e começo do js

---
+++ Calculadora/script.js
@@ -0,0 +1,34 @@
+const operacao = document.getElementById("operacao")
+
+function somar(){
+
+}
+
+function subtrair(){
+    
+}
+
+function multiplicar(){
+    
+}
+
+function dividir(){
+    
+}
+
+function limpar(){
+    //limpa todos os numeros
+}
+
+function excluir(){
+    //Retira o ultimo numero
+}
+
+function somar(){
+    
+}
+
+function inserir(numero){
+    operacao.innerHTML += numero
+    console.log(operacao)
+}
