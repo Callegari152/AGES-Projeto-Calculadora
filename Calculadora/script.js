4ee14335ef1fe64b39d962af1d1161b6184cf17c
Luca <lucacallegari71@gmail.com>
2025-08-18 16:39:54 -0300

    https://github.com/Callegari152/AGES-Projeto-Calculadora/blob/main/README.md

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,19 +1,46 @@
 const operacao = document.getElementById("operacao")
-
+const operacaoP = document.getElementById("operacaoP")
 
 function inserir(numero){
-    operacao.innerHTML += numero
-    console.log(operacao)
+    if (operacao.innerHTML[operacao.innerHTML.length - 1] == " "){
+        if(numero == " + " || numero == " - " || numero == " * " || numero == " / "){
+            operacao.innerHTML = operacao.innerHTML.substring(0, operacao.innerHTML.length - 3)
+            operacao.innerHTML += numero;
+            console.log(numero)
+        } else {
+            operacao.innerHTML += numero;
+        }
+    } else {
+        operacao.innerHTML += numero
+        console.log(operacao.innerHTML[operacao.innerHTML.length - 1])
+    }
 }
 
 function limparP(){
-    operacao.innerHTML = operacao.innerHTML.substring(0, operacao.innerHTML.length - 1)
+    if(operacao.innerHTML[operacao.innerHTML.length - 1] == " ") {
+        operacao.innerHTML = operacao.innerHTML.substring(0, operacao.innerHTML.length - 3)
+    } else {
+        operacao.innerHTML = operacao.innerHTML.substring(0, operacao.innerHTML.length - 1)
+    }
 }
 
 function limparT(){
     operacao.innerHTML = "";
+    operacaoP.innerHTML = "";
 }
 
-function resultado(){
-    
+function parenteses(){
+    if(operacao.innerHTML.includes('(')){
+        operacao.innerHTML = operacao.innerHTML + ")"
+    } else {
+        operacao.innerHTML = operacao.innerHTML + "("
+    }
 }
+
+
+function resultado(){
+    operacaoP.innerHTML = operacao.innerHTML;
+    operacao.innerHTML = operacao.innerHTML.replace('%', '/100').replace("," , ".");    
+    operacao.innerHTML = Number(eval(operacao.innerHTML)).toString().replace("." , ",");
+}
+
